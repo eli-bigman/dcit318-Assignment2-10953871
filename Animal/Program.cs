1e3997cf7612345c2e0bae00e866ebe956916f60
eli-bigman <nutsugah81@gmail.com>
2024-06-17 10:35:08 +0000

    create animal class

--- Animal/Program.cs
+++ Animal/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace dcit318_assignment2_10953871
+{
+    internal class AnimalProgram
+    {
+        static void Main(string[] args)
+        {
+            //Dog and Cat objects are created and their makeSound methods are called
+            Dog josephTheDog = new Dog();
+            josephTheDog.makeSound();
+
+            Cat josephTheCat = new Cat();
+            josephTheCat.makeSound();
+        }
+
+    }
+
+    public class Animal
+    {
+        public virtual void makeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+
+    public class Dog : Animal
+    {
+        public override void makeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void makeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+}
