ac104d8830f1e15582e77285156304b09af8aa7a
eli-bigman <nutsugah81@gmail.com>
2024-06-17 16:38:08 +0000

    add rectangle class

--- Shape/Program.cs
+++ Shape/Program.cs
@@ -1,16 +1,55 @@
-﻿namespace Shape
+﻿using System.Transactions;
+
+namespace Shape
 {
     internal class ShapeProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the radius of the circle");
-            string radiusInput = Console.ReadLine();
-            if (double.TryParse(radiusInput, out double radius))
+            Console.WriteLine("""
+                Select a shape
+                1. Circle
+                2. Rectangle
+
+                """);
+
+            string choice = Console.ReadLine()!;
+            switch (choice)
             {
-                Circle circle = new Circle(radius);
-                circle.GetArea();
+                case "1":
+                    Console.WriteLine("Enter the radius of the circle");
+                    string radiusInput = Console.ReadLine();
+                    if (double.TryParse(radiusInput, out double radius))
+                    {
+                        Circle circle = new Circle(radius);
+                        circle.GetArea();
+                    }
+                    break;
+                case "2":
+                    
+                    Console.WriteLine("Enter the lenght and breadth of the rectangle");
+                    Console.WriteLine("Enter lenght");
+                    string lenghtInput = Console.ReadLine()!;
+                    Console.WriteLine("Enter breadth");
+                    string breadthInput = Console.ReadLine()!;
+
+                    if (double.TryParse(lenghtInput, out double lenght) && double.TryParse(breadthInput, out double breadth))
+                    {
+                        Rectangle rectangle = new Rectangle(lenght, breadth);
+                        rectangle.GetArea();
+                    }
+                    else {                         
+                        Console.WriteLine("Invalid input, Please enter a number");
+                    }
+                    break;
+
+                default:  
+                    Console.WriteLine("Invalid choice");
+                   break;
             }
+
+
+            
             
         }
     }
@@ -34,6 +73,24 @@
         {
             double area = Math.PI * _radius * _radius;
             Console.WriteLine($"The area of the circle with radius {_radius} is {area}");
+        }
+    }
+
+    public class Rectangle: Shape
+    {
+        private double _length, _breadth;
+
+        public Rectangle(double length, double breadth)
+        {
+            _breadth = breadth;
+            _length = length;
+
+        }
+
+        public override void GetArea()
+        {
+           double area = _length * _breadth;
+            Console.WriteLine($"Area of the rectangle is {area}");
         }
     }
 }
