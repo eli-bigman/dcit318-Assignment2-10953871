7269c80e0147b04ed74dcb0036e8ddab3a5b04fb
eli-bigman <nutsugah81@gmail.com>
2024-06-17 11:58:09 +0000

    add shape class

--- Shape/Program.cs
+++ Shape/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Shape
+{
+    internal class ShapeProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the radius of the circle");
+            string radiusInput = Console.ReadLine();
+            if (double.TryParse(radiusInput, out double radius))
+            {
+                Circle circle = new Circle(radius);
+                circle.GetArea();
+            }
+            
+        }
+    }
+
+    public abstract class Shape
+    {
+        public abstract void GetArea();
+    }
+
+    public class Circle : Shape
+    {
+        private double _radius;
+
+        public Circle(double radius)
+        {
+            _radius = radius;
+
+        }
+
+        public override void GetArea()
+        {
+            double area = Math.PI * _radius * _radius;
+            Console.WriteLine($"The area of the circle with radius {_radius} is {area}");
+        }
+    }
+}
