dcc7ff74f5a2c2ca78d597bd77446dc66d44ec5a
eli-bigman <nutsugah81@gmail.com>
2024-06-17 17:38:08 +0000

    create and implement IMovable interface

--- Move/Program.cs
+++ Move/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Move
+{
+    public class MoveProgram
+    {
+
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Move();
+
+            Bicycle bicycle = new Bicycle();
+            bicycle.Move();
+
+        }
+
+        internal interface IMovable
+        {
+            void Move();
+        }
+
+        public class Car() : IMovable
+        {
+            public void Move()
+            {
+                Console.WriteLine("Car is moving");
+            }
+        }
+
+        public class Bicycle()
+        {
+            public void Move()
+            {
+                Console.WriteLine("Bicycle is moving");
+            }
+        }
+    }
+      
+}
